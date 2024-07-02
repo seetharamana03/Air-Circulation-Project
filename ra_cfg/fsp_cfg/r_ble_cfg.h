bd502a2baa68854ed06c3eefa5cbf91e0bb20977
anandseetharaman <145276108+anandseetharaman@users.noreply.github.com>
2024-07-02 14:05:30 -0400

    Add files via upload

---
+++ ra_cfg/fsp_cfg/r_ble_cfg.h
@@ -0,0 +1,17 @@
+/* generated configuration header file - do not edit */
+#ifndef R_BLE_CFG_H_
+#define R_BLE_CFG_H_
+#ifdef __cplusplus
+            extern "C" {
+            #endif
+
+#define BLE_CFG_DA14xxx_DEVICE              1
+#define BLE_CFG_RESET_PIN                   (BSP_IO_PORT_07_PIN_08)
+#define BLE_CFG_RESET_POLARITY              (00)
+#define BLE_CFG_RTS_PIN                     (BSP_IO_PORT_04_PIN_12)
+#define BLE_CFG_HOST_BOOT_MODE             (1)
+
+#ifdef __cplusplus
+            }
+            #endif
+#endif /* R_BLE_CFG_H_ */
---
+++ ra_cfg/fsp_cfg/rm_ble_abs_gtl_cfg.h
@@ -0,0 +1,25 @@
+/* generated configuration header file - do not edit */
+#ifndef RM_BLE_ABS_GTL_CFG_H_
+#define RM_BLE_ABS_GTL_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BLE_ABS_CFG_RF_CONNECTION_MAXIMUM           (1)
+#define BLE_ABS_CFG_GATT_MTU_SIZE                   (247)
+#define BLE_ABS_CFG_NUMBER_BONDING                  (1)
+#define BLE_ABS_CFG_TIMER_NUMBER_OF_SLOT            (10)
+#define BLE_ABS_CFG_PARAM_CHECKING_ENABLE           (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define BLE_CFG_RF_CONN_MAX                         (1)
+#define BLE_ABS_CFG_TRANSMIT_POWER_INDEX            (34)
+
+#if RA_NOT_DEFINED > 0
+            #define RM_BLE_ABS_GTL_TRANSPORT_INTERFACE_SPI
+#else
+#define RM_BLE_ABS_GTL_TRANSPORT_INTERFACE_UART
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* RM_BLE_ABS_GTL_CFG_H_ */
